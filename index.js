8da0f0af49c983ff252b4372ec4bcd531093f9d5
Adhuy <yudhamerdeka91@gmail.com>
2025-09-19 18:54:19 +0700

    Add API generate from document and audio

--- index.js
+++ index.js
@@ -15,14 +15,15 @@ const ai = new GoogleGenAI({});
 const geminiModels = {
   text: "gemini-2.5-flash-lite",
   image: "gemini-2.5-flash",
+  document: "gemini-2.5-flash",
   audio: "gemini-2.5-flash",
-  document: "gemini-2.5-flash-lite",
 };
 
 // insialisasi aplikasi backend/server
 app.use(cors()); // mengizinkan request dari semua origin
 app.use(express.json()); // mengizinkan request dengan format JSON
 
+// generate text
 app.post('/generate-text', async (req, res) => {
   try {
     const { body } = req;
@@ -46,6 +47,7 @@ app.post('/generate-text', async (req, res) => {
   }
 });
 
+// generate dari gambar
 app.post('/generate-from-image', upload.single('image'), async (req, res) => {
   try {
     const { prompt } = req.body;
@@ -75,6 +77,69 @@ app.post('/generate-from-image', upload.single('image'), async (req, res) => {
   }finally {
     // hapus file setelah digunakan
     fs.unlink(req.file.path);
+  }
+});
+
+// generate dari dokumen
+app.post('/generate-from-document', upload.single('document'), async (req, res) => {
+  try {
+    const { prompt = "deskripsikan dokumen berikut" } = req.body;
+    const filePath = req.file.path;
+    const buffer = await fs.readFile(filePath);
+    const base64Data = buffer.toString('base64');
+    const mimeType = req.file.mimetype;
+    
+    // baca dokumen
+    const documentPart = {
+      inlineData: { data: base64Data, mimeType },
+    };
+
+    // upload dokumen
+    const resp = await ai.models.generateContent({
+      model: geminiModels.document,
+      contents: [
+        createUserContent([
+          prompt,
+          documentPart
+        ]),
+      ]
+    });
+
+    res.json({ output: resp.text });
+  }catch (err){
+    res.status(500).json({ error: err.message });
+  }finally {
+    // hapus file setelah digunakan
+    fs.unlink(req.file.path);
+  }
+});
+
+// generate dari audio
+app.post('/generate-from-audio', upload.single('audio'), async (req, res) => {
+  try {
+    const { prompt = "deskripsikan audio berikut" } = req.body;
+    const audioPath = req.file.path;
+    const buffer = await fs.readFile(audioPath);
+    const base64Audio = buffer.toString('base64');
+    const mimeType = req.file.mimetype;
+    // baca audio
+    const audioPart = {
+      inlineData: { data: base64Audio, mimeType },
+    };
+
+    // upload audio
+    const resp = await ai.models.generateContent({
+      model: geminiModels.audio,
+      contents: [
+        createUserContent([
+          prompt,
+          audioPart
+        ]),
+      ]
+    });
+    res.json({ output: resp.text });
+  }catch (err){
+    res.status(500).json({ error: err.message });
   }
 });
 
